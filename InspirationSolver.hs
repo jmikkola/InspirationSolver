b32d3de51ddae6a4bdac6ec760d0ad7a9c874af7
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-02-07 16:11:52 -0800

    Cleanup

--- InspirationSolver.hs
+++ InspirationSolver.hs
@@ -1,8 +1,7 @@
-
-
 import Data.List (permutations)
 
 isDivisibleBy :: Int -> Int -> Bool
+isDivisibleBy _ 0 = False
 isDivisibleBy a b = (mod a b) == 0
 
 data Operator = Plus | Minus | Times | Divide
@@ -12,10 +11,12 @@ data Trial = Trial [Operator] [Int]
            deriving (Eq, Show)
 
 apply :: Operator -> Int -> Int -> Maybe Int
-apply Plus a b  = Just (a + b)
-apply Minus a b = Just (a - b)
-apply Times a b = Just (a * b)
-apply Divide a b = if isDivisibleBy a b then Just (div a b) else Nothing
+apply Plus   a b = Just (a + b)
+apply Minus  a b = Just (a - b)
+apply Times  a b = Just (a * b)
+apply Divide a b = if isDivisibleBy a b
+                   then Just (div a b)
+                   else Nothing
 
 computeTrial :: Trial -> Maybe Int
 computeTrial (Trial ops nums) = do
@@ -31,10 +32,10 @@ concatEach :: [a] -> [a] -> [[a]]
 concatEach concats to = map (\c -> c : to) concats
 
 combinations :: Int -> [a] -> [[a]]
-combinations n items = if n < 2
-                       then map list items
-                       else concatMap (concatEach items) (combinations (n - 1) items)
-
+combinations n items =
+  if n < 2
+  then map list items
+  else concatMap (concatEach items) (combinations (n - 1) items)
 
 operatorCombinations = combinations 3 allOperators
 
@@ -42,7 +43,10 @@ allOrderings :: [Int] -> [[Int]]
 allOrderings = permutations
 
 allTrials :: [Int] -> [Trial]
-allTrials nums = [Trial ops num | ops <- operatorCombinations, num <- allOrderings nums]
+allTrials nums = [
+  Trial ops num
+  | ops <- operatorCombinations, num <- allOrderings nums
+  ]
 
 find :: [Trial] -> Int -> Maybe Trial
 find []     _ = Nothing
@@ -53,5 +57,4 @@ find (t:ts) n = case computeTrial t of
 compute :: Int -> [Int] -> Maybe Trial
 compute n nums = find (allTrials nums) n
 
---main = putStrLn $ show $ compute 3 [1,3,4,7]
 main = putStrLn $ show $ compute 4 [1,2,3,5]
